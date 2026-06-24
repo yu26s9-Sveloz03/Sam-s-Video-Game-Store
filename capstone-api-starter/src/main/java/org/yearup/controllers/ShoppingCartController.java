dad91e2ba4ef7c6e69b7202e5f21902227b67474
Sveloz03 <velozsam@gmail.com>
2026-06-23 20:18:51 -0400

    implemented shopping cart GET and POST methods

--- capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,5 +1,9 @@
 package org.yearup.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 import org.yearup.service.ShoppingCartService;
@@ -8,16 +12,23 @@ import org.yearup.service.UserService;
 import java.security.Principal;
 
 // convert this class to a REST controller
+@RestController
+@RequestMapping("/cart")
 // only logged in users should have access to these actions
+@PreAuthorize("isAuthenticated()")
 public class ShoppingCartController
 {
     // a shopping cart controller depends on the service layer
     private ShoppingCartService shoppingCartService;
     private UserService userService;
 
-
+    public ShoppingCartController(ShoppingCartService shoppingCartService, UserService userService){
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+    }
 
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping
     public ShoppingCart getCart(Principal principal)
     {
         // get the currently logged in username
@@ -27,11 +38,19 @@ public class ShoppingCartController
         int userId = user.getId();
 
         // use the shoppingCartService to get all items in the cart and return the cart
-        return null;
+        return shoppingCartService.getByUserId(userId);
     }
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be added)
+    @PostMapping("/products/{productId}")
+    public ResponseEntity<ShoppingCart> addToCart(Principal principal, @PathVariable int productId){
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+        ShoppingCart shoppingCart = shoppingCartService.addToCart(userId,productId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(shoppingCart);
+    }
     // return the updated cart with status 201 Created
 
 
--- capstone-api-starter/src/main/java/org/yearup/service/ShoppingCartService.java
+++ capstone-api-starter/src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,8 +1,14 @@
 package org.yearup.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.yearup.models.CartItem;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.repository.ShoppingCartRepository;
+
+import java.util.List;
 
 @Service
 public class ShoppingCartService
@@ -20,8 +26,33 @@ public class ShoppingCartService
     public ShoppingCart getByUserId(int userId)
     {
         // load the user's cart rows, look up each product, and build the ShoppingCart
-        return null;
+        ShoppingCart shoppingCart = new ShoppingCart();
+        List<CartItem> cartItems = shoppingCartRepository.findByUserId(userId);
+        for (CartItem cartItem :cartItems) {
+            Product product = productService.getById(cartItem.getProductId());
+            ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+            shoppingCartItem.setProduct(product);
+            shoppingCartItem.setQuantity(cartItem.getQuantity());
+            shoppingCart.add(shoppingCartItem);
+        }
+
+        return shoppingCart;
     }
 
     // add additional methods here
+
+    public ShoppingCart addToCart(int userId, int productId) {
+        CartItem cartItem = shoppingCartRepository.findByUserIdAndProductId(userId, productId);
+        ShoppingCart shoppingCart = (ShoppingCart) shoppingCartRepository.findByUserId(userId);
+        if (cartItem == null) {
+            Product product = productService.getById(productId);
+            ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+            shoppingCartItem.setProduct(product);
+            shoppingCartItem.setQuantity(1);
+            shoppingCart.add(shoppingCartItem);
+        } else {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        }
+        return shoppingCart;
+    }
 }
